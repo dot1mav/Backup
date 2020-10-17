b2d9ce9851b1e566a8ceebedce2d47083a2c4e74
Ma V <44059142+dot1mav@users.noreply.github.com>
2020-10-18 00:35:27 +0330

    give arg

--- clt.py
+++ clt.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     print('> get croc command you resive from srv')
     get_command:str = input('> ')
     clt: Conn = Conn()
-    clt.Clt_Connection()
+    clt.Clt_Connection(get_command)
