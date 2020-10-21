9e3d744132ac9ec5c03a4044f54d8bf1458adc7f
Ma V <44059142+dot1mav@users.noreply.github.com>
2020-10-21 08:11:09 +0330

    add ^C

--- srv.py
+++ srv.py
@@ -60,27 +60,27 @@ if __name__ == "__main__":
         print(f'pls create dir and db file...')
         exit(0)
     create_temp()
-    with open('dir.txt', 'r') as dir_file:
-        for item in dir_file:
-            dir_list.append(str(item).replace('\n', ''))
-        dir_file.close()
-    with open('db.txt', 'r') as db_file:
-        for item in db_file:
-            db_list.append(str(item).replace('\n', ''))
-        db_file.close()
-
-    print(f'{db_list}\n{dir_list}')
-
-    for directory in tqdm(dir_list, desc='Backup Directory...'):
-        make_backup_dir(directory)
-
-    for db_name in tqdm(db_list, desc='Backup Database...'):
-        make_backup_db(db_name)
-
-    make_7z_backup()
-    srv: Conn = Conn()
-    os.system('clear')
     try:
+        with open('dir.txt', 'r') as dir_file:
+            for item in dir_file:
+                dir_list.append(str(item).replace('\n', ''))
+            dir_file.close()
+        with open('db.txt', 'r') as db_file:
+            for item in db_file:
+                db_list.append(str(item).replace('\n', ''))
+            db_file.close()
+
+        print(f'{db_list}\n{dir_list}')
+
+        for directory in tqdm(dir_list, desc='Backup Directory...'):
+            make_backup_dir(directory)
+
+        for db_name in tqdm(db_list, desc='Backup Database...'):
+            make_backup_db(db_name)
+
+        make_7z_backup()
+        srv: Conn = Conn()
+        os.system('clear')
         srv.Srv_Connection(BACKUP_7Z_NAME)
     except KeyboardInterrupt:
         clear_temp()
